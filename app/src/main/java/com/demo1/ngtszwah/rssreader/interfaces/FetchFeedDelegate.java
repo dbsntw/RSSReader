4c2815162770b58d08aebcf8f9ef069bae13ffd5
Philip Ng <ntw.box@gmail.com>
2018-12-05 03:41:11 +0800

    Removes ignored files again

--- app/src/main/java/com/demo1/ngtszwah/rssreader/interfaces/FetchFeedDelegate.java
+++
@@ -1,9 +0,0 @@
-package com.demo1.ngtszwah.rssreader.interfaces;
-
-import com.demo1.ngtszwah.rssreader.models.Feed;
-
-import java.io.InputStream;
-
-public interface FetchFeedDelegate {
-    void fetchFeedFinished(Feed feed);
-}
--- app/src/main/java/com/demo1/ngtszwah/rssreader/interfaces/OnItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.demo1.ngtszwah.rssreader.interfaces;
-
-import com.demo1.ngtszwah.rssreader.models.FeedItem;
-
-public interface OnItemClickListener {
-    void onItemClick(FeedItem item);
-}
